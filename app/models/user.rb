4deca3b16fd4e376595bcb8fdca63e1c1ff31fd5
Adam <electronicbattleweapon7@gmail.com>
2014-01-16 00:19:57 +0000

    github auth stuffs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,22 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  def self.find_for_github_oauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.save!
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 PrView::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { 
+    :omniauth_callbacks => "users/omniauth_callbacks" 
+  }
+
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   def get(path,controller,name,as = name)
     match path => controller + '#' + name, :as => as, :via => [:get]
